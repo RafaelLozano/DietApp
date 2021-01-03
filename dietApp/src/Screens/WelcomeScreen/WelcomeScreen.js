ed3c85868940d8717a6aedb59d7365d7a109bc32
Rafael Lozano <rafa.lozano.rolon@gmail.com>
2021-01-03 13:29:52 -0600

    Android fix

--- dietApp/src/Screens/WelcomeScreen/WelcomeScreen.js
+++ dietApp/src/Screens/WelcomeScreen/WelcomeScreen.js
@@ -8,41 +8,43 @@ const WelcomeScreen = () => {
     return (
         <View styles={styles.mainContainer}>
             <ImageBackground style={styles.background} source={require('../../../assets/images/BackgroundScreen1a.png')}>
-            
-            </ImageBackground>
-            <Image 
-                source={require('../../../assets/images/WelcomeImage1.jpeg')}
-            style={styles.image}
-            />
-            <View style={{backgroundColor:'white'}}> 
-                <View style={styles.textContainer}>
-                    <Text style={styles.title}>Best tips for your diets</Text>
-                    <Text numberOfLines={3} style={styles.subTitle}> 
-                        Lorem ipsum dolor sit amet consectetur adipiscing elit, ante elementum praesent pharetra cras class cubilia, 
-                        ultrices hac id bibendum porttitor integer. Convallis phasellus auctor fringilla blandit est congue consequat scelerisque, 
-                        curabitur metus potenti faucibus orci eu sollicitudin condimentum eros, aptent dis dapibus cras lacinia vitae sagittis.
-                        </Text>
+                <View style={{ height: '70%', width: '100%', zIndex: -1,}}>
+                    <Image
+                        source={require('../../../assets/images/WelcomeImage1.jpeg')}
+                        style={styles.image}
+                    />
                 </View>
-                <View style={styles.buttonContainer}>
-                    {/* BOTON 1 */}
-                    <Button 
-                    content={"Skip step"} 
-                    color={"secundary"} 
-                    onPress={() => {
-                            navigator.navigate('Login')
-                        }}
-                    />
-                    <Button 
-                    content={"Next"} 
-                    color={"primary"} 
-                    onPress={() => {
-                            navigator.navigate('Login')
-                        }}
-                    />
 
-                    {/* BOTON 2 */}
+                <View style={{ backgroundColor: 'transparent' }}>
+                    <View style={{ width: '90%', backgroundColor: 'transparent',marginTop:10,marginBottom:20}}>
+                        <Text style={styles.title}>Best tips for your diets</Text>
+                        <Text numberOfLines={3} style={styles.subTitle}>
+                            Lorem ipsum dolor sit amet consectetur adipiscing elit, ante elementum praesent pharetra cras class cubilia,
+                            ultrices hac id bibendum porttitor integer. Convallis phasellus auctor fringilla blandit est congue consequat scelerisque,
+                            curabitur metus potenti faucibus orci eu sollicitudin condimentum eros, aptent dis dapibus cras lacinia vitae sagittis.
+                        </Text>
+                    </View>
+                    <View style={{flexDirection:'row',justifyContent:'space-around',alignItems:'center'}}>
+                        {/* BOTON 1 */}
+                        <Button
+                            content={"Skip step"}
+                            color={"secundary"}
+                            onPress={() => {
+                                navigator.navigate('Login')
+                            }}
+                        />
+                        <Button
+                            content={"Next"}
+                            color={"primary"}
+                            onPress={() => {
+                                navigator.navigate('Login')
+                            }}
+                        />
+
+                    </View>
                 </View>
-            </View>
+            </ImageBackground>
+            
         </View>
     )
 }
--- dietApp/src/Screens/WelcomeScreen/styles.js
+++ dietApp/src/Screens/WelcomeScreen/styles.js
@@ -1,47 +1,43 @@
 import { StyleSheet } from 'react-native'
 //color = #58be75
 const styles = StyleSheet.create({
+    mainContainer: {
+        height: '100%',
+        width: '100%',
+        flex: 1,
+        
+    },
+    background: {
+        height: '100%',
+        width: '100%',
+        alignItems:'center',
+        alignContent:'space-between'
+    },
     image:{
-        height:'75%',
-        width:'100%',
-        zIndex:-1,
-        position:'absolute',
-        
-    },
-    mainContainer:{
-        
         height:'100%',
-        backgroundColor:'red',
+        width:'100%',
+        resizeMode:'cover',
+        justifyContent:'center',
+        alignItems:'center',
+        position:'absolute',
     },
-    textContainer:{
-        marginHorizontal:40,
-        color:'white',
-    },
+    
     title:{
         fontSize:25,
         textAlign:'center',
-        marginVertical:20,
         fontWeight:'bold',
         color:'#201f33',
-    },
-    
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        top: 50,
-        backgroundColor:'white',
-    },
-    background:{
-        height: '75%',
-        width: '100%',
-        zIndex:10,
-        top:'20%'
+        marginBottom:10,
+        paddingVertical:10
         
     },
+    
     subTitle: {
         color: 'grey',
         textAlign: 'center',
+        marginBottom:12,
     },
+    
     
     
 })
